49e395611bce490ac64dcc7beaea4d90ed017aa2
Marcus Trepte <marcus.trepte@elevait.de>
2024-09-13 14:30:38 +0200

    add comment string plugin

--- init.lua
+++ init.lua
@@ -20,6 +20,7 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   'nvim-tree/nvim-web-devicons',
+  require 'custom.plugins.commentstring',
   require 'custom.plugins.markdown-viewer',
   require 'custom.plugins.code-editing',
   require 'custom.plugins.debug',
