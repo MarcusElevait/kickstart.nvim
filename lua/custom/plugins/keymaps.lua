78f0a6832ab1a760b9a60286c056abe8524ddb30
Marcus Trepte <marcus.trepte@elevait.de>
2024-09-13 11:24:55 +0200

    add markdown viewer

--- lua/custom/plugins/keymaps.lua
+++ lua/custom/plugins/keymaps.lua
@@ -42,3 +42,5 @@ vim.keymap.set('n', '<C-Up>', ':resize -2<CR>')
 vim.keymap.set('n', '<C-Down>', ':resize +2<CR>')
 vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>')
 vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>')
+
+vim.keymap.set('n', '<C-G>', ':Glow<CR>')
